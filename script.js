f60603c53f61f4a3baeae0452c37fbf4ca663c77
kashifjavaid1 <gmdcf797@gmail.com>
2023-05-06 21:54:47 +0500

    table create in loop

---
+++ script.js
@@ -0,0 +1,9 @@
+// var table=+prompt("table");
+// for (var abc = 1; table < abc.length; abc++) {
+//     console.log(table,"*",abc,"=",table*abc);
+    
+// }
+var table = +prompt("Enter a number for the table:");
+for (var abc = 1; abc <= 10; abc++) {
+    console.log(table, "*",abc,'=',table*abc);
+}
